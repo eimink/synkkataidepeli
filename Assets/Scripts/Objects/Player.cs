92b10adba661422fc6ff7288159dd4a6762c0975
Failattu <kalle.sirkesalo@eficode.com>
2017-07-21 22:16:00 +0300

    Moved health management to player.cs

--- Assets/Scripts/Objects/Player.cs
+++ Assets/Scripts/Objects/Player.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-    GameObject o;
     private Rigidbody2D rb;
     public float speed;
+    private float nextActionTime = 0.0f;
+    public float period = 6.0f;
+    public float health = 100.0f;
+    private float tickDMG = 0.1f;
 
     void Start()
     {
-        o = (GameObject)this.gameObject;
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -19,5 +21,21 @@ public class Player : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement * speed);
+
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+            health = loseHP(tickDMG);
+            // execute block of code here
+        }
+    }
+    public float loseHP(float amount)
+    {
+        return health -= amount;
+    }
+
+    public float gainHP(float amount)
+    {
+        return health -= amount;
     }
 }
