402f2aadda97853dddbe8f2ce464fe0ba4e4bc4b
Teppo Kauppinen <teppo.kauppinen@traplightgames.com>
2017-07-21 20:07:28 +0300

    removed obsolete code

--- Assets/Scripts/LevelGenerator/RandomLevelGenerator.cs
+++ Assets/Scripts/LevelGenerator/RandomLevelGenerator.cs
@@ -23,12 +23,6 @@ public class RandomLevelGenerator : LevelGenerator {
 	void Start () {
 		Init();
 		GenerateWithSeed (seed);
-		/*if (SceneHelper.instance != null)
-		{
-			
-		}
-		else
-			RunGenerator();*/
 	}
 
 	void RunGenerator()
