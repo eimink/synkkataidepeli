bd4b8dd4f001c142a6a792f43447b166fdf25588
Failattu <kalle.sirkesalo@eficode.com>
2017-07-22 16:30:21 +0300

    Started work on unit tests

---
+++ Assets/Tests/Editor/EditModeTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class EditModeTest {
+
+	[Test]
+	public void EditModeTestSimplePasses() {
+        // Use the Assert class to test conditions.
+        Assert.AreEqual(true, true);
+    }
+    [Test]
+    public void GameObject_CreatedWithGiven_WillHaveTheName()
+    {
+        var go = new GameObject("dickbutt");
+        Assert.AreEqual("dickbutt", go.name);
+    }
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+	public IEnumerator EditModeTestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+}
