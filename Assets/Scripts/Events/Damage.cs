14565832346cfcde02835866eef97e89db762268
Failattu <kalle.sirkesalo@eficode.com>
2017-07-22 12:52:44 +0300

    Added friendly dickbutt to compensate for enemydickbutt, added dickbutt to help test this, moved damage from onCollision to events folder and gained

---
+++ Assets/Scripts/Events/Damage.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour {
+
+    public float HPLost;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("HP granted");
+            col.gameObject.SendMessage("loseHP", HPLost);
+        }
+    }
+}
