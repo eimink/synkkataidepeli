748fa3cc3afca72f36e10c787cfdd5182942ca61
Failattu <kalle.sirkesalo@eficode.com>
2017-07-22 14:23:34 +0300

    Added inventory scripts and reactingwithplayer script to manage the doors

---
+++ Assets/Scripts/Events/ReactingWithPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactingWithPlayer : MonoBehaviour {
+    public string keyName;
+
+    private GameObject player;
+    private Inventory checkItem;
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindWithTag("Player");
+        checkItem = (Inventory)player.GetComponent(typeof(Inventory));
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (checkItem.getInventory(keyName)) {
+                SetAllCollidersStatus(false);
+            }
+        }
+    }
+    private void SetAllCollidersStatus(bool active)
+    {
+        foreach (Collider c in GetComponents<Collider>())
+        {
+            c.enabled = active;
+        }
+    }
+}
