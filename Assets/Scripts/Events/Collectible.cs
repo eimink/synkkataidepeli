5ae09de85b9418eba920d429b56a3e674af3259a
Failattu <kalle.sirkesalo@eficode.com>
2017-07-22 13:14:16 +0300

    Simplified the code to be used more versatily. Also helps us create better prefabs.

---
+++ Assets/Scripts/Events/Collectible.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class integerChange : MonoBehaviour
+{
+
+    public float intChange;
+    public bool staysActive = false;
+    public string methodToCall;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            col.gameObject.SendMessage(methodToCall, intChange);
+            gameObject.SetActive(staysActive);
+        }
+    }
+}
+
