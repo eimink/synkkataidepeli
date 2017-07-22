3c5b7e7a2d8386a471fac26b2d5e00a058b0bdc1
Failattu <kalle.sirkesalo@eficode.com>
2017-07-22 11:10:48 +0300

    Health HUD first version where Dickbutt flashes when damage is granted. Also Image that flashes uses dickbutt. TODO: Change slider to look dickbutts

--- Assets/Scripts/Objects/Player.cs
+++ Assets/Scripts/Objects/Player.cs
@@ -1,20 +1,44 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D rb;
     public float speed;
-    private float nextActionTime = 0.0f;
     public float period = 6.0f;
     public float health = 100.0f;
-    private float tickDMG = 0.1f;
+    public Image damageImage;
+    public float flashSpeed = 5f;
+    public Slider healthSlider;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
 
+    private float tickDMG = 0.1f;
+    private int HPChange;
+    private float nextActionTime = 0.0f;
+    private Rigidbody2D rb;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
+    void Update()
+    {
+        // If the player has just been damaged...
+        if (HPChange == 2)
+        {
+            // ... set the colour of the damageImage to the flash colour.
+            damageImage.color = flashColour;
+        }
+        // Otherwise...
+        else
+        {
+            // ... transition the colour back to clear.
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
 
+        // Reset the damaged flag.
+        HPChange = 0;
+    }
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -31,11 +55,20 @@ public class Player : MonoBehaviour
     }
     public float loseHP(float amount)
     {
-        return health -= amount;
+        health -= amount;
+        HPChange = 2;
+        healthSlider.value = health;
+        return health;
+
+
     }
 
     public float gainHP(float amount)
     {
-        return health -= amount;
+        health += amount;
+        HPChange = 1;
+        healthSlider.value = health;
+        return health;
+
     }
 }
