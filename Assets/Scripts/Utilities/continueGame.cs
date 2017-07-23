e96b018acf927eb574e0c69303e5ea92be2a99a2
Teppo Kauppinen <teppo.kauppinen@traplightgames.com>
2017-07-23 03:27:21 +0300

    Dickbutt AM!

---
+++ Assets/Scripts/Utilities/continueGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class continueGame : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Mouse1"))
+		{
+			SceneManager.LoadScene("test",LoadSceneMode.Single);
+		}
+	}
+}
