6e77962e4ebc1e6d934f839668c4cadf3170347f
Failattu <kalle.sirkesalo@eficode.com>
2017-07-23 12:30:42 +0300

    Made the game fuck ton harder

--- Assets/Scripts/Objects/Player.cs
+++ Assets/Scripts/Objects/Player.cs
@@ -14,7 +14,7 @@ public class Player : MonoBehaviour
     private Vector3 movement;
     private Color flashDMGColor = new Color(1f, 0f, 1f, 0.1f);
     private Color flashHPColor = new Color(0f, 1f, 0f, 0.1f);
-    private float tickDMG = 0.1f;
+    private float tickDMG = 5.0f;
     private int HPChange;
     private bool isDead;
     private float nextActionTime = 0.0f;
