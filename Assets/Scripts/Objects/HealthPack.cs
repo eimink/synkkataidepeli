82f86e39c94281f45915c0502bbc30570da5bf31
Teppo Kauppinen <teppo.kauppinen@traplightgames.com>
2017-07-23 12:27:06 +0300

    will you have coffee or cheese?

---
+++ Assets/Scripts/Objects/HealthPack.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HealthPack : MonoBehaviour {
+
+	public void set(bool coffee) {
+		if (!coffee)
+			this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Textures/healt_cheese");
+		else
+			this.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Textures/health_coffee");
+	}
+
+}
